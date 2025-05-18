ab790bd41c2d050d9cd96a879c56ec0122290935
Alex Iraola <alex.iraola@gmail.com>
2025-05-18 22:17:59 +0200

    Main describe

--- src/tests/e2e/status.test.ts
+++ src/tests/e2e/status.test.ts
@@ -8,99 +8,101 @@ dotenv.config({
   path: process.env.NODE_ENV === 'test' ? '.test.env' : '.env'
 });
 
-describe('Status endpoint', () => {
-  let server: Server;
+describe("Orders API", () => {
+  describe('Status endpoint', () => {
+    let server: Server;
 
-  beforeAll(() => {
-    const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
-    const PORT = process.env.PORT || "3001";
-    server = createServer(DB_URL, PORT);
+    beforeAll(() => {
+      const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
+      const PORT = process.env.PORT || "3001";
+      server = createServer(DB_URL, PORT);
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it('checks API health', async () => {
+      const response = await request(server).get("/");
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ status: 'ok' });
+    });
   });
 
-  afterAll(() => {
-    server.close();
+  describe("POST /orders", () => {
+    let server: Server;
+
+    beforeAll(() => {
+      const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
+      const PORT = process.env.PORT || "3001";
+      server = createServer(DB_URL, PORT);
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it('should create an order', async () => {
+      const response = await createValidOrder(server);
+
+      expect(response.status).toBe(200);
+      expect(response.text).toEqual("Order created with total: 10");
+    });
+
+    it('creates an order with discount code', async () => {
+      const response = await createValidOrder(server, 'DISCOUNT20');
+
+      expect(response.status).toBe(200);
+      expect(response.text).toEqual("Order created with total: 8");
+    });
+
+    it('does not create an order without items', async () => {
+      const order = {
+        items: [],
+        shippingAddress: '123 Main St, Anytown, USA'
+      };
+
+      const response = await request(server).post("/orders").send(order);
+
+      expect(response.status).toBe(400);
+      expect(response.text).toEqual("The order must have at least one item");
+    });
   });
 
-  it('checks API health', async () => {
-    const response = await request(server).get("/");
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({ status: 'ok' });
-  });
-});
+  describe('GET /orders', () => {
+    let server: Server;
 
-describe("POST /orders", () => {
-  let server: Server;
+    beforeAll(async () => {
+      const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
+      const PORT = process.env.PORT || "3001";
+      server = createServer(DB_URL, PORT);
+      await mongoose.connection.dropDatabase();
+    });
 
-  beforeAll(() => {
-    const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
-    const PORT = process.env.PORT || "3001";
-    server = createServer(DB_URL, PORT);
-  });
+    afterEach(async () => {
+      await mongoose.connection.dropDatabase();
+    });
 
-  afterAll(() => {
-    server.close();
-  });
+    afterAll(() => {
+      server.close();
+    });
 
-  it('should create an order', async () => {
-    const response = await createValidOrder(server);
+    it('should get empty array when no orders exist', async () => {
+      const response = await request(server).get("/orders");
 
-    expect(response.status).toBe(200);
-    expect(response.text).toEqual("Order created with total: 10");
-  });
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual([]);
+    });
 
-  it('creates an order with discount code', async () => {
-    const response = await createValidOrder(server, 'DISCOUNT20');
+    it('should get all orders', async () => {
+      await createValidOrder(server);
+      await createValidOrder(server);
 
-    expect(response.status).toBe(200);
-    expect(response.text).toEqual("Order created with total: 8");
-  });
+      const response = await request(server).get("/orders");
 
-  it('does not create an order without items', async () => {
-    const order = {
-      items: [],
-      shippingAddress: '123 Main St, Anytown, USA'
-    };
-
-    const response = await request(server).post("/orders").send(order);
-
-    expect(response.status).toBe(400);
-    expect(response.text).toEqual("The order must have at least one item");
-  });
-});
-
-describe('GET /orders', () => {
-  let server: Server;
-
-  beforeAll(async () => {
-    const DB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/db_orders";
-    const PORT = process.env.PORT || "3001";
-    server = createServer(DB_URL, PORT);
-    await mongoose.connection.dropDatabase();
-  });
-
-  afterEach(async () => {
-    await mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    server.close();
-  });
-
-  it('should get empty array when no orders exist', async () => {
-    const response = await request(server).get("/orders");
-
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual([]);
-  });
-
-  it('should get all orders', async () => {
-    await createValidOrder(server);
-    await createValidOrder(server);
-
-    const response = await request(server).get("/orders");
-
-    expect(response.status).toBe(200);
-    expect(response.body.length).toBe(2);
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(2);
+    });
   });
 });
 
