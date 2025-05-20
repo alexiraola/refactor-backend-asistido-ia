c587fc753c2db05a847dd0ac755a62227b1682d0
Alex Iraola <alex.iraola@gmail.com>
2025-05-20 15:49:20 +0200

    Discount value object

---
+++ src/domain/valueObjects/discount.ts
@@ -0,0 +1,14 @@
+export class Discount {
+  private constructor(private readonly code: string, private readonly value: number) { }
+
+  static fromCode(code: string) {
+    if (code === 'DISCOUNT20') {
+      return new Discount(code, 0.8);
+    }
+    return new Discount(code, 1);
+  }
+
+  apply(price: number) {
+    return price * this.value;
+  }
+}
---
+++ src/tests/unit/domain/valueObjects/discount.spec.ts
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest';
+import { Discount } from '../../../../domain/valueObjects/discount';
+
+describe('Discount', () => {
+  it('should create a discount from a valid code', () => {
+    const discount = Discount.fromCode('DISCOUNT20');
+    expect(discount).toBeInstanceOf(Discount);
+  });
+
+  it('should apply the discount to a price', () => {
+    const discount = Discount.fromCode('DISCOUNT20');
+
+    expect(discount.apply(100)).toBe(80);
+    expect(discount.apply(200)).toBe(160);
+  });
+
+  it('should not apply the discount to a price if the code is invalid', () => {
+    const discount = Discount.fromCode('INVALID');
+
+    expect(discount.apply(100)).toBe(100);
+  });
+});
